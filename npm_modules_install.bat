dd8f4211e562225ac02ff85373804a22d94ee199
nathan <nathanseux@gmail.com>
2020-03-27 18:12:22 +0100

    change modules install file

--- npm_modules_install.bat
+++ npm_modules_install.bat
@@ -5,6 +5,7 @@ call npm install socket.io
 call npm install body-parser
 call npm install socket.io
 call npm install fs
+call npm install nodemon
 call npm install fs-extra
 call npm install csv-parser
 call npm install csv-writer
