2d333a25465eff122b3dfca4f6f0e647bc8ed1bf
nathan <nathanseux@gmail.com>
2020-03-27 18:10:59 +0100

    Add modules install file

---
+++ npm_modules_install.bat
@@ -0,0 +1,12 @@
+call npm install express
+call npm install ejs
+call npm install serialport
+call npm install socket.io
+call npm install body-parser
+call npm install socket.io
+call npm install fs
+call npm install fs-extra
+call npm install csv-parser
+call npm install csv-writer
+call npm install child_process
+pause
